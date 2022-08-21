93b3780d9b024f0384ac20ef56671916be52de1e
Andrés Gómez <af.gomezg1@uniandes.edu.co>
2022-08-21 02:01:04 -0500

    Punto 5 shell

---
+++ Taller1_Metodos/Shell_5_variaciones.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+function factorial()
+{
+	local producto=1
+	for i in $(seq 1 1 $2)
+	do	
+		producto=$(($producto*$i))
+	done
+	let $1=$producto	
+}
+
+nfact=0
+dfact=0
+
+factorial nfact 20
+factorial dfact 17
+
+echo $(($nfact/$dfact)) 
