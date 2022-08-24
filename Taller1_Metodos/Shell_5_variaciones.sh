1e775779ab0ccdd194786008a49182276ec492eb
Andrés Gómez <af.gomezg1@uniandes.edu.co>
2022-08-23 22:39:18 -0500

    Punto 6 y cambio punto 5 shell

--- Taller1_Metodos/Shell_5_variaciones.sh
+++ Taller1_Metodos/Shell_5_variaciones.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-function factorial()
-{
-	local producto=1
-	for i in $(seq 1 1 $2)
-	do
-		producto=$(($producto*$i))
-	done
-	let $1=$producto
-}
-
-nfact=0
-dfact=0
-
-factorial nfact 20
-factorial dfact 17
-
 echo $(($nfact/$dfact))
+
 #TODO
 #Punto 6
-#La complejidad es
+"""
+Como en el ejercicio anterior se puede simplificar la fórmula V20_3 = 20!/(20-3)! a V20_3 = 20*19*18*17!/(17!) y,
+posteriormente, a V20_3 = 20*19*18, la complejidad corresponde a dos multiplicaciones, es decir, la complejidad es
+O(n^0 (r-1)^1) = O(r-1) puesto que, en el caso del punto 6, r-1 = 3-1 = 2 que es igual a la cantidad de operaciones necesarias
+para resolver la variación.
+"""
