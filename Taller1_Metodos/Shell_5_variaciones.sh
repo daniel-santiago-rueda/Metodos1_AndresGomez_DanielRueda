2e8f5b0f1344fca26839365b898c71b73fcb1353
Andrés Gómez <af.gomezg1@uniandes.edu.co>
2022-08-23 22:41:20 -0500

    Arreglo punto 6 shell

--- Taller1_Metodos/Shell_5_variaciones.sh
+++ Taller1_Metodos/Shell_5_variaciones.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-echo $(($nfact/$dfact))
+echo $(( 20*19*18 ))
 
 #TODO
 #Punto 6
-"""
-Como en el ejercicio anterior se puede simplificar la fórmula V20_3 = 20!/(20-3)! a V20_3 = 20*19*18*17!/(17!) y,
-posteriormente, a V20_3 = 20*19*18, la complejidad corresponde a dos multiplicaciones, es decir, la complejidad es
-O(n^0 (r-1)^1) = O(r-1) puesto que, en el caso del punto 6, r-1 = 3-1 = 2 que es igual a la cantidad de operaciones necesarias
-para resolver la variación.
-"""
+
+#Como en el ejercicio anterior se puede simplificar la fórmula V20_3 = 20!/(20-3)! a V20_3 = 20*19*18*17!/(17!) y,
+#posteriormente, a V20_3 = 20*19*18, la complejidad corresponde a dos multiplicaciones, es decir, la complejidad es
+#O(n^0 (r-1)^1) = O(r-1) puesto que, en el caso del punto 6, r-1 = 3-1 = 2 que es igual a la cantidad de operaciones necesarias
+#para resolver la variación.
