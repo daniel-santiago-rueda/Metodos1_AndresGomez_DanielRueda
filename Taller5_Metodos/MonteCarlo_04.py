a225edf41db15a5e2ebacbfa35ca155fa20600c7
daniel-santiago-rueda <d.ruedav@uniandes.edu.co>
2022-09-24 21:09:14 -0500

    Ejercicio 4 OK

--- Taller5_Metodos/MonteCarlo_04.py
+++ Taller5_Metodos/MonteCarlo_04.py
@@ -2,15 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-N = int(10**4)
-x = np.random.uniform(0,100,N)
+N = 10**4
+x = np.random.rand(N)
 
 def Correlations(k):
     sum_ = 0
-    for i in range(N):
+    for i in range(N-k):
         sum_ += (x[i]*x[i+k])
-        return sum_/N
+    return sum_/N
 
-k = np.array(range(0, 30))
-for i in k:
-    print(Correlations(i))
+K = np.zeros(30)
+Y = np.zeros_like(K)
+
+for i in range(1,31):
+    K[i-1] = i
+    Y[i-1] = Correlations(i)
+
+plt.plot(K, Y)
+plt.title("Generador \'rand\' de Numpy")
+plt.xlabel('k-ésimo vecino')
+plt.ylabel('C(k)')
+plt.ylim((0.2, 0.3))
+plt.show()
